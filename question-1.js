5c80b166dfc03c10c017c78ab88d15b90add9a40
PHET45 <phnjk2000@gmail.com>
2025-07-23 13:57:03 +0700

    Implement todo list modifications: add new item, update existing item, remove last item, and log results

--- question-1.js
+++ question-1.js
@@ -6,3 +6,9 @@ const myTodo = [
   { id: 4, todo: "Wash dishes" },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+let add = {id: 5, todo: "Walk the dog"};
+  myTodo.push(add); //1.เพิ่ม Object ใหม่เข้าไปใน myTodo ซึ่งมี id เป็น 5 และมี todo เป็น "Walk the dog"
+  myTodo[3].todo = "Go to the gym"; //2) แก้ไขชื่อ Object ที่มี id เป็น 4 เพื่อให้ todo ของ Object นั้นมีค่าเป็น "Go to the gym"
+  myTodo.pop(); //3) ลบ Object สุดท้ายใน Array ออก
+  console.log(myTodo); //4) นำ myTodo มาแสดงบนหน้าจอ Console
+  console.log(`To-do id: ${myTodo[3].id},${myTodo[3].todo}`);
